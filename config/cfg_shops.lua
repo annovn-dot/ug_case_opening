1c79934bd66c1ee17c531e329c4182e3e74af1b1
annovn-dot <55518126+annovn-dot@users.noreply.github.com>
2025-11-25 03:18:28 +0100

    Update cfg_shops.lua

--- config/cfg_shops.lua
+++ config/cfg_shops.lua
@@ -1,10 +1,10 @@
 Config.Shops = {
     {
-        id       = 'case_shop_performance',
+        id       = 'case_shop',
         coords   = vec3(123.7346, -3028.1768, 6.8),
         size     = vec3(1.4, 1.4, 1.6),
         rotation = 0.0,
-        label    = 'Performance case shop',
+        label    = 'Case shop',
         icon     = 'fa-solid fa-box-open',
         cases    = {
             'case_engine',
@@ -17,3 +17,4 @@ Config.Shops = {
         },
     }
 }
+
